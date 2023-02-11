1f7b6b9a4cabf74daae376a9c5a57af318b76edf
Tsebal <atsebal@gmail.com>
2023-02-11 17:28:50 +0300

    Refactored: CityDAOImpl.

--- src/main/java/ru/javarush/module4/projecthibernatefinal/dao/CityDAOImpl.java
+++ src/main/java/ru/javarush/module4/projecthibernatefinal/dao/CityDAOImpl.java
@@ -3,9 +3,11 @@ package ru.javarush.module4.projecthibernatefinal.dao;
 import ru.javarush.module4.projecthibernatefinal.entity.City;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CityDAOImpl {
 
+    Optional<City> getById(Integer id);
     List<City> getItems(int offset, int limit);
 
     int getTotalCount();
